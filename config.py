1d45cbfc333b1f20767122d6af31c938062ae3b2
interlitoral <jcolombara@interlitoral.com.ar>
2025-05-29 19:07:04 -0300

    Agrego identificador clientes

---
+++ config.py
@@ -0,0 +1,14 @@
+CONEXIONES = {    
+    "1": { #Interlitoral
+        "server": "INTERLITORAL",
+        "database": "Movint",
+        "username": "Axoft",
+        "password": "Axoft"
+    },
+    "cliente2": {
+        "server": "ip_o_dns_cliente2",
+        "database": "db_cliente2",
+        "username": "user2",
+        "password": "pass2"
+    }
+}
